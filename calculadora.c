a32c02ee1a09f315bff0ec10a6ebec90ece380d4
BrenoJoly <brenojoly27@gmail.com>
2026-04-12 14:37:51 -0300

    versao final

--- calculadora.c
+++ calculadora.c
@@ -4,7 +4,5 @@ int main() {
     int a = 10, b = 5;
     printf("Soma: %d\n", a + b);
     printf("Subtracao: %d\n", a - b);
-    printf("Multi: %d\n", a * b);
-    printf("Div: %d\n", a / b);
     return 0;
 }
